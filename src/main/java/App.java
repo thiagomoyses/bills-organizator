53d2dc108942efcbd8cdb61666f2076f7ac54fd3
THIAGO MOYSES <thiagodasilvamoysesfr@gmail.com>
2025-10-01 17:21:08 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class App {
+
+    private static void createAndShowGuiExample() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("TopLevelDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create the menu bar.  Make it have a green background.
+        JMenuBar greenMenuBar = new JMenuBar();
+        greenMenuBar.setOpaque(true);
+        greenMenuBar.setBackground(new Color(154, 165, 127));
+        greenMenuBar.setPreferredSize(new Dimension(200, 20));
+
+        //Create a yellow label to put in the content pane.
+        JLabel yellowLabel = new JLabel();
+        yellowLabel.setOpaque(true);
+        yellowLabel.setBackground(new Color(248, 213, 131));
+        yellowLabel.setPreferredSize(new Dimension(200, 180));
+
+        //Set the menu bar and add the label to the content pane.
+        frame.setJMenuBar(greenMenuBar);
+        frame.getContentPane().add(yellowLabel, BorderLayout.CENTER);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGuiExample();
+            }
+        });
     }
 }
